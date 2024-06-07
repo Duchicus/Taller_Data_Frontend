9c888fdc132bbcb35d788f2b9fc45098f47cd8ea
BrunoMalfi <malfi.bruno@gmail.com>
2024-06-07 09:16:20 +0200

    updates axios not working still

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
-import PieChart from '../charts/chartGender/chartGender';
+import PieChart from '../Charts/ChartGender/ChartGender';
+import { useDispatch, useSelector } from 'react-redux'
+//import { salesDataAll} from '../features/chart/chartSlice.js';
 
 const data = [
   { name: 'A', value: 30 },
@@ -10,6 +12,11 @@ const data = [
 ];
 
 const Home = () => {
+  // const {dataAll} = useSelector((state)=>state.dataAll)
+  // const dispatch = useDispatch()
+  // dispatch(salesDataAll())
+  // console.log('data :', dataAll);
+
   return (
     <div>
       <h1>Grafico de Tarta con D3.js y React</h1>
